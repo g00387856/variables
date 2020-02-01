eafd8dda7ccfa81105ebe6f7939401a307cc9d1a
unknown <g00387856@gmit.ie>
2020-02-01 07:28:13 +0000

    Added new file for variables.

---
+++ Variables.py
@@ -0,0 +1,17 @@
+# Michelle Moran
+#write a program that asks user to input a string and outputs every second letter in reverse
+# first to create a simple variable
+
+x = 911
+y = 2
+z = x + y
+print(z)
+
+# Week 3 task - write a program thats asks to input a string and out
+#every second letter in reverse
+S= " The grass is not always greener"
+#stating the sentence to read in reverse -1
+S = S [::-1]
+#instructing python to read only every 2nd letter 
+
+print (S[::2])
